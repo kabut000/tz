414abe5801ac2476a0f06371a9d17f2d70e1e8fc
kabut000 <kabuto64225@gmail.com>
2022-01-21 16:16:08 +0900

    SPSR_EL3.M=EL1h

--- run.sh
+++ run.sh
@@ -2,14 +2,9 @@
 
 qemu-system-aarch64 -M virt,secure=on \
 	-cpu cortex-a53 -m 1024 \
-	-serial tcp:localhost:54320 -serial tcp:localhost:54321 \
 	-nographic \
 	-initrd rootfs.cpio.gz \
 	-bios flash.bin \
 	-kernel Image \
+	-serial tcp:localhost:54320 -serial tcp:localhost:54321 \
     # -s -S 
-
-# qemu-system-aarch64 -M virt,secure=on \
-# 	-cpu cortex-a53 -m 1024 \
-# 	-nographic \
-# 	-kernel QEMU_EFI.fd 
